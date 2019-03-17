357fb36bb1fc565f741facba81f21b1961e998d8
TranHuynhThanhTai <thanhtai171998@gmail.com>
2019-03-17 15:41:43 +0700

    Test nhanh branch

--- Jquery/app/models/DanhSachNguoiDung.js
+++ Jquery/app/models/DanhSachNguoiDung.js
@@ -1,3 +1,6 @@
 function DanhSachNguoiDung() {
     this.mangNguoiDung = [];
 }
+// Nếu là người mời git hub thì trc khi git push thì phải git pull về
+// git branch Tạo nhánh git
+// git checkout Thay đổi nhánh hiện tại sang nhánh muốn chuyển
