0d59f325ddda636268cc9e5d8be4d2bd66bde198
TranHuynhThanhTai <thanhtai171998@gmail.com>
2019-03-17 15:58:14 +0700

    test checkout

--- Jquery/app/models/DanhSachNguoiDung.js
+++ Jquery/app/models/DanhSachNguoiDung.js
@@ -4,3 +4,4 @@ function DanhSachNguoiDung() {
 // Nếu là người mời git hub thì trc khi git push thì phải git pull về
 // git branch Tạo nhánh git
 // git checkout Thay đổi nhánh hiện tại sang nhánh muốn chuyển
+// Lần đầu tạo branch => Phải dùng: lệnh git push --set-upstream origin feature-ThanhTai
